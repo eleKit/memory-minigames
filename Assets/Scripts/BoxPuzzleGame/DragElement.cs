b8d2acad2dd3903873a03591683e39a1ca4d0ea5
Eleonora Chitti <eleonora.chitti@mail.polimi.it>
2021-09-03 17:44:15 +0200

    added drag script

---
+++ Assets/Scripts/BoxPuzzleGame/DragElement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class DragElement : MonoBehaviour
+{
+ 
+
+    private void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            this.GetComponent<Transform>().position = curPosition;
+
+
+    }
+
+
+
+}
